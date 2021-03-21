b067820e7154d62fd1f01d4e51064b4ba0a0a135
Kshitiz Gupta <kshitiz.gupta@izettle.com>
2021-03-21 20:07:05 +0100

    Coffee machine service definition

--- src/main/java/com/kshitiz/coffeeMachine/service/CoffeeMachineService.java
+++ src/main/java/com/kshitiz/coffeeMachine/service/CoffeeMachineService.java
@@ -1,4 +1,10 @@
 package com.kshitiz.coffeeMachine.service;
 
+import com.kshitiz.coffeeMachine.model.Beverage;
+import java.util.Set;
+
 public interface CoffeeMachineService {
+    void makeBeverage(final Beverage beverage);
+    void refill(final String ingredient, final int qty);
+    Set<String> getLowIngredients();
 }
